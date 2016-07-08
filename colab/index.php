71927c012cf322f9eb7cba3975c2cfc1f44033f4
Pedro Semeano <semeano@gmail.com>
2016-07-08 16:59:32 +0100

    Main page theme.

--- colab/index.php
+++ colab/index.php
@@ -1,9 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Colab</title>
+</head>
+<body>
+
+<?php get_header(); ?>
+
 <?php
 
-	$sites = wp_get_sites();
+	$sites = wp_get_sites(array(
+    'network_id' => null,
+    'public'     => null,
+    'archived'   => 0,
+    'mature'     => 0,
+    'spam'       => 0,
+    'deleted'    => 0,
+    'limit'      => 100,
+    'offset'     => 0
+	));
 
-	foreach ( $sites as $site ) {
-		print $post['path'];
+	for ($i = 0; $i < count($sites); $i++) {
+		if ($sites[$i]['path'] !== "/") {
+			echo '<a href="' . $sites[$i]['path'] . '">' . $sites[$i]['path'] . '</a><br>';
+		}
 	}
 
 ?>
+
+<br>
+<a href="wp-signup.php">Create a new project</a>
+
+<?php get_footer(); ?>
+
+</body>
+</html>
